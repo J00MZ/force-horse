4896bda3e7e18b252f5839f834e603ee527d3a72
Joe <jtavin@gmail.com>
2014-09-08 16:44:17 +0300

    Create popup.js
    The real action happens here ;-)

---
+++ popup.js
@@ -0,0 +1,2 @@
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("force", "g"), "horse");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Force", "g"), "Horse");
