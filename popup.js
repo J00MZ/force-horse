3163500a334b4aa566c76407a071d619db8e14f8
Joe Tavin <jtavin@gmail.com>
2014-09-09 16:35:35 +0300

    added usage for "Forcing" "Horsing"

--- popup.js
+++ popup.js
@@ -1,2 +1,4 @@
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("force", "g"), "horse");
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Force", "g"), "Horse");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("force", "gi"), "horse");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Force", "gi"), "Horse");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("forci", "gi"), "horsi");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Forci", "gi"), "Horsi");
